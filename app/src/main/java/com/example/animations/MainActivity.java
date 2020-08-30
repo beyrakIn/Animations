bbf30e4a8dea681b7b0984cb00e3a51c17e53c53
beyrakmsc <beyraklnc@gmail.com>
2020-08-30 17:59:27 +0400

    Initial commit

---
+++ app/src/main/java/com/example/animations/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.animations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
